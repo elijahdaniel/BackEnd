1f00752cf864946f92bf632fe54e10e0071bee11
Elijah Paminsan <e.paminsan@gmail.com>
2020-01-08 20:22:13 -0800

    testing id

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -43,9 +43,11 @@ router.post('/login', (req, res) => {
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user)
-        res
-          .status(200)
-          .json({ message: `Welcome ${user.username}!`, token: token })
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token: token,
+          id: `${user.id}`
+        })
       } else {
         res.status(401).json({ message: 'Invalid Credentials' })
       }
