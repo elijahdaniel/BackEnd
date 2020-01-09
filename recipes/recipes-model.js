5bdcf17b38de605ab3402a83177d19be733ba35d
Elijah Paminsan <e.paminsan@gmail.com>
2020-01-09 12:14:48 -0800

    added an endpoint for ingredients

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -1,36 +1,51 @@
-const db = require('../database/dbConfig.js');
+const db = require('../database/dbConfig.js')
 
 module.exports = {
   getRecipes,
+  getIngredients,
   getById,
   add,
   update,
-  remove,
-};
+  remove
+}
 
 function getRecipes() {
   return db('recipes as r')
-  .join('chefs as c', 'c.id','r.chef_id')
-  .select('c.avatar_url','r.title','r.description','r.instructions','r.meal_type','r.pic_url');
-}
-function getById(id) {
-  return db('recipes').where({ id });
+    .join('chefs as c', 'c.id', 'r.chef_id')
+    .select(
+      'c.avatar_url',
+      'r.title',
+      'r.description',
+      'r.instructions',
+      'r.meal_type',
+      'r.pic_url'
+    )
 }
 
-function add(recipes){
-  return db('recipes')
-  .insert(recipes, ['id'])
+function getIngredients() {
+  return db('ingredients').select(
+    'ingredients.id',
+    'ingredients.ingredient_name'
+  )
+}
+
+function getById(id) {
+  return db('recipes').where({ id })
+}
+
+function add(recipes) {
+  return db('recipes').insert(recipes, ['id'])
   //.then(([id]) => id)
 }
 
 function update(id, changes) {
   return db('recipes')
     .where({ id })
-    .update(changes);
+    .update(changes)
 }
 
 function remove(id) {
   return db('recipes')
     .where({ id })
-    .del();
+    .del()
 }
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -1,80 +1,89 @@
-const express = require('express');
+const express = require('express')
 
-const Recipes = require('./recipes-model.js');
+const Recipes = require('./recipes-model.js')
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/', (req, res) => {
-  Recipes
-    .getRecipes()
+  Recipes.getRecipes()
     .then(recipes => {
-      res.status(200).json(recipes);
+      res.status(200).json(recipes)
     })
     .catch(error => {
-      res.status(500);
-    });
-});//endpoint works
+      res.status(500)
+    })
+}) //endpoint works
+
+router.get('/ingredients', (req, res) => {
+  Recipes.getIngredients()
+    .then(ingredient => {
+      res.status(200).json(ingredient)
+    })
+    .catch(error => {
+      res.status(500)
+    })
+}) //endpoint works
 
 router.get('/:id', async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
 
-  Recipes
-    .getById(id)
+  Recipes.getById(id)
     .then(recipe => {
       if (recipe) {
-        res.json(recipe);
+        res.json(recipe)
       } else {
-        res.status(404).json({ message: 'Could not find recipe with given id.' });
+        res
+          .status(404)
+          .json({ message: 'Could not find recipe with given id.' })
       }
     })
     .catch(err => {
-      res.status(500).json({ message: 'Failed to get recipe' });
-    });
-});//endpoint works
-
+      res.status(500).json({ message: 'Failed to get recipe' })
+    })
+}) //endpoint works
 
 router.post('/', async (req, res) => {
-  const recipeData = req.body;
+  const recipeData = req.body
   try {
-  await Recipes.add(recipeData);
-    res.status(201).json({ message: "recipe created" });
+    await Recipes.add(recipeData)
+    res.status(201).json({ message: 'recipe created' })
   } catch (err) {
     console.log(err)
-    res.status(500).json({ message: 'Failed to create new recipe', err });
+    res.status(500).json({ message: 'Failed to create new recipe', err })
   }
-});//endpoint works
+}) //endpoint works
 
 router.put('/:id', async (req, res) => {
-  const { id } = req.params;
-  const changes = req.body;
+  const { id } = req.params
+  const changes = req.body
 
   try {
-    const updatedRecipe = await Recipes.update(id, changes);
+    const updatedRecipe = await Recipes.update(id, changes)
 
     if (updatedRecipe) {
-      res.json({ update: updatedRecipe });
+      res.json({ update: updatedRecipe })
     } else {
-      res.status(404).json({ message: 'Could not find recipe with given id' });
+      res.status(404).json({ message: 'Could not find recipe with given id' })
     }
   } catch (err) {
-    res.status(500).json({ message: 'Failed to update recipe' });
+    res.status(500).json({ message: 'Failed to update recipe' })
   }
-});//endpoint works
+}) //endpoint works
 
 router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
 
   try {
-    const deleted = await Recipes.remove(id);
+    const deleted = await Recipes.remove(id)
 
     if (deleted) {
-      res.json({ removed: id });
+      res.json({ removed: id })
     } else {
-      res.status(404).json({ message: 'Could not find recipe with given id' });
+      res.status(404).json({ message: 'Could not find recipe with given id' })
     }
   } catch (err) {
-    res.status(500).json({ message: 'Failed to delete recipe' });
+    res.status(500).json({ message: 'Failed to delete recipe' })
   }
-});
+})
 
-module.exports = router;
+module.exports = router
